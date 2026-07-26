ddb4fd982376b3e051b7f03f1f12f03827df7d69
Thijs van Waaij <thijs@vanwaaij.com>
2026-07-26 20:08:11 +0200

    fix isLinux

--- modules/base/framework.nix
+++ modules/base/framework.nix
@@ -8,7 +8,7 @@
   # `programs.fw-fanctrl` only exists once the fw-fanctrl module is imported.
   # On darwin (or any non-Framework host) the module isn't loaded, so emit no
   # assignments at all rather than guarded ones.
-  config = lib.optionalAttrs (pkgs.stdenv.hostPlatform.isLinux && isFramework) {
+  config = lib.optional (pkgs.stdenv.hostPlatform.isLinux && isFramework) {
     hardware.fw-fanctrl = {
       enable = true;
       config.defaultStrategy = "medium";
