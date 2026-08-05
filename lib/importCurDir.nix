64ec3e55348ed8b0bacf4cccd542ca3ad6dec4c3
Thijs van Waaij <thijs@vriend.studio>
2026-08-05 14:31:59 +0200

    fixes

--- lib/importCurDir.nix
+++ lib/importCurDir.nix
@@ -1,6 +1,10 @@
 lib: dir:
-map (n: _: toString dir + "/${n}") (
-  lib.filterAttrs (name: _: (lib.strings.hasSuffix ".nix" name) && (name != "default.nix")) (
-    builtins.readDir dir
-  )
+let
+  isModule =
+    name: type:
+    (type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix")
+    || (type == "directory" && builtins.pathExists (dir + "/${name}/default.nix"));
+in
+lib.mapAttrs' (name: _: lib.nameValuePair (lib.removeSuffix ".nix" name) (dir + "/${name}")) (
+  lib.filterAttrs isModule (builtins.readDir dir)
 )
